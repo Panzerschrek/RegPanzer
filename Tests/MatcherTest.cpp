a9c89a7e70b39ea9e84ea2ae5a017eaf0b50408b
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-04-17 13:12:08 +0300

    Dopolnenije nabora testov.

--- Tests/MatcherTest.cpp
+++ Tests/MatcherTest.cpp
@@ -66,29 +66,96 @@ const TestDataElement c_test_data[]
 			},
 			{ // String is equal to regexp.
 				"abc",
-				{
-					{ 0, 3 },
-				},
+				{ { 0, 3 }, },
 			},
 			{ // Signle match in middle of string.
 				"lolQabcwat",
-				{
-					{ 4, 7 },
-				},
+				{ { 4, 7 }, },
 			},
 			{ // Signle match at end of string.
 				"012345abc",
-				{
-					{ 6, 9 },
-				},
+				{ { 6, 9 }, },
 			},
 			{ // Multiple ranges in string.
 				"abcSSabcQ",
-				{
-					{ 0, 3 }, { 5, 8 },
-				},
+				{ { 0, 3 }, { 5, 8 }, },
 			},
 		},
+	},
+
+	// Match sequence of same symbols.
+	{
+		"Q+",
+		{
+			{ // Empty string - no matches.
+				"",
+				{},
+			},
+			{ // Non-empty string - no matches.
+				"WWWAaSD",
+				{},
+			},
+			{ // Single match - whole string with one length.
+				"Q",
+				{ {0, 1}, }
+			},
+			{ // Single match - whole string with multiple symbols.
+				"QQQQQQQ",
+				{ {0, 7}, }
+			},
+			{ // Single match in middle of string.
+				"aaQQbb",
+				{ {2, 4}, }
+			},
+			{ // Single match in end of string.
+				"fffFQQQ",
+				{ {4, 7}, }
+			},
+			{ // Multiple matches.
+				"wawdQwdawdQQQwafawfQQfffQQQQa",
+				{ {4, 5}, {10, 13}, {19, 21}, {24, 28}, }
+			},
+		}
+	},
+
+	// Match sequence of symobl ranges.
+	{
+		"[a-z]+",
+		{
+			{ // Empty string - no matches.
+				"",
+				{},
+			},
+			{ // Non-empty string - no matches.
+				"@6656%%1",
+				{},
+			},
+			{ // No matches for symobls outside range.
+				"CAPITAL LETTERS",
+				{},
+			},
+			{ // Single match for single symobl in range.
+				"f",
+				{ {0, 1}, }
+			},
+			{ // Single match for single word.
+				"leftwing",
+				{ {0, 8}, }
+			},
+			{ // Single match for single word in middle of string.
+				"66 cups!",
+				{ {3, 7}, }
+			},
+			{ // Multiple words.
+				"quick brown fox jumps over the lazy dog",
+				{ {0, 5}, {6, 11}, {12, 15}, {16, 21}, {22, 26}, {27, 30}, {31, 35}, {36, 39} }
+			},
+			{
+				// Non-words at begin and at end.
+				"45lol-wat6",
+				{ {2, 5}, {6, 9} }
+			},
+		}
 	},
 };
 
