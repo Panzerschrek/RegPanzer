804b7f9dc6ea5c8e94d527ca8e4dadea87c54cf5
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-04-24 20:37:41 +0300

    Napisany funkçii preobrazovanija linejnoj struktury regulärnyh vyraženij v grafopodobnuju strukturu.

--- RegPanzerLib/RegexGraph.cpp
+++ RegPanzerLib/RegexGraph.cpp
@@ -1,5 +1,160 @@
 #include "RegexGraph.hpp"
+#include <cassert>
 
 namespace RegPanzer
 {
+
+namespace
+{
+
+using RegexChainIterator= RegexElementsChain::const_iterator;
+
+
+void SetMostRightNext(GraphElements::Node& node, const GraphElements::NodePtr& next);
+
+template<typename T>
+void SetMostRightNextImpl(T& node, const GraphElements::NodePtr& next)
+{
+	if(node.next == nullptr)
+		node.next= next;
+	else
+		SetMostRightNext(*node.next, next);
+}
+
+void SetMostRightNextImpl(GraphElements::Alternatives& node, const GraphElements::NodePtr& next)
+{
+	for(GraphElements::NodePtr& alternative : node.next)
+	{
+		if(alternative == nullptr)
+			alternative= next;
+		else
+			SetMostRightNext(*alternative, next);
+	}
+}
+
+void SetMostRightNextImpl(GraphElements::LoopCounterBlock& node, const GraphElements::NodePtr& next)
+{
+	if(node.next_loop_end == nullptr)
+		node.next_loop_end= next;
+	else
+		SetMostRightNext(*node.next_loop_end, next);
+}
+
+void SetMostRightNext(GraphElements::Node& node, const GraphElements::NodePtr& next)
+{
+	std::visit([&](auto& el){ return SetMostRightNextImpl(el, next); }, node);
+}
+
+GraphElements::NodePtr BuildRegexGraphImpl(const RegexChainIterator begin, const RegexChainIterator end);
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const AnySymbol&)
+{
+	return std::make_shared<GraphElements::Node>(GraphElements::AnySymbol{});
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const SpecificSymbol& specific_symbol)
+{
+	return std::make_shared<GraphElements::Node>(GraphElements::SpecificSymbol{nullptr, specific_symbol.code});
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const OneOf& one_of)
+{
+	GraphElements::OneOf out_node;
+	out_node.variants= one_of.variants;
+	out_node.ranges= one_of.ranges;
+	out_node.inverse_flag= one_of.inverse_flag;
+	return std::make_shared<GraphElements::Node>(std::move(out_node));
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const Group& group)
+{
+	const auto group_end= std::make_shared<GraphElements::Node>(GraphElements::GroupEnd{nullptr, group.index});
+	const auto group_contents= BuildRegexGraphImpl(group.elements.begin(), group.elements.end());
+	SetMostRightNext(*group_contents, group_end);
+
+	return std::make_shared<GraphElements::Node>(GraphElements::GroupStart{group_contents, group.index});
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const BackReference& back_reference)
+{
+	return std::make_shared<GraphElements::Node>(GraphElements::BackReference{nullptr, back_reference.index});
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const NonCapturingGroup& non_capturing_group)
+{
+	return BuildRegexGraphImpl(non_capturing_group.elements.begin(), non_capturing_group.elements.end());
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const AtomicGroup& atomic_group)
+{
+	// TODO
+	(void)atomic_group;
+	assert(false);
+	return nullptr;
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const Alternatives& alternatives)
+{
+	GraphElements::Alternatives out_node;
+	for(const auto& alternative : alternatives.alternatives)
+		out_node.next.push_back(BuildRegexGraphImpl(alternative.begin(), alternative.end()));
+
+	return std::make_shared<GraphElements::Node>(std::move(out_node));
+}
+
+GraphElements::NodePtr BuildRegexGraphNodeImpl(const Look& look)
+{
+	// TODO
+	(void)look;
+	assert(false);
+	return nullptr;
+}
+
+GraphElements::NodePtr BuildRegexGraphNode(const RegexElementFull::ElementType& element)
+{
+	return std::visit([&](const auto& el){ return BuildRegexGraphNodeImpl(el); }, element);
+}
+
+GraphElements::NodePtr BuildRegexGraphImpl(const RegexChainIterator begin, const RegexChainIterator end)
+{
+	if(begin == end)
+		return nullptr;
+
+	const RegexElementFull& element= *begin;
+
+	const auto node= BuildRegexGraphNode(element.el);
+	const auto next_node= BuildRegexGraphImpl(std::next(begin), end);
+
+	if(element.seq.min_elements == 1 && element.seq.max_elements == 1)
+	{
+		SetMostRightNext(*node, next_node);
+		return node;
+	}
+	else
+	{
+		const GraphElements::LoopId id= &element;
+
+		const auto loop_counter_block=
+			std::make_shared<GraphElements::Node>(
+				GraphElements::LoopCounterBlock{
+					node,
+					next_node,
+					id,
+					element.seq.min_elements,
+					element.seq.max_elements,
+					element.seq.mode != SequenceMode::Lazy,
+					});
+
+		SetMostRightNext(*node, loop_counter_block);
+		return std::make_shared<GraphElements::Node>(GraphElements::LoopEnter{loop_counter_block, id});
+	}
+}
+
+} // namespace
+
+GraphElements::NodePtr BuildRegexGraph(const RegexElementsChain& regex_chain)
+{
+	return BuildRegexGraphImpl(regex_chain.begin(), regex_chain.end());
+}
+
 } // namespace RegPanzer
--- RegPanzerLib/RegexGraph.hpp
+++ RegPanzerLib/RegexGraph.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "RegexElements.hpp"
 #include <memory>
 #include <variant>
 #include <vector>
@@ -14,13 +15,25 @@ using CharType= char32_t;
 struct AnySymbol;
 struct SpecificSymbol;
 struct OneOf;
+struct Alternatives;
+struct GroupStart;
+struct GroupEnd;
+struct BackReference;
+struct LoopEnter;
+struct LoopCounterBlock;
 
 using Node= std::variant<
 	AnySymbol,
 	SpecificSymbol,
-	OneOf>;
+	OneOf,
+	Alternatives,
+	GroupStart,
+	GroupEnd,
+	BackReference,
+	LoopEnter,
+	LoopCounterBlock>;
 
-using NodePtr= std::shared_ptr<const Node>;
+using NodePtr= std::shared_ptr<Node>;
 
 struct AnySymbol
 {
@@ -65,21 +78,26 @@ struct BackReference
 	size_t index= std::numeric_limits<size_t>::max();
 };
 
+using LoopId= const void*;
+
 struct LoopEnter
 {
 	NodePtr next; // To loop counter block.
-	size_t index= std::numeric_limits<size_t>::max();
+	LoopId id= nullptr;
 };
 
 struct LoopCounterBlock
 {
 	NodePtr::weak_type next_iteration;
 	NodePtr next_loop_end;
-	size_t index= std::numeric_limits<size_t>::max();
+	LoopId id= nullptr;
 	size_t min_elements= 0u;
 	size_t max_elements= 0u;
+	bool greedy= true;
 };
 
 } // GraphElements
+
+GraphElements::NodePtr BuildRegexGraph(const RegexElementsChain& regex_chain);
 
 } // namespace RegPanzer
