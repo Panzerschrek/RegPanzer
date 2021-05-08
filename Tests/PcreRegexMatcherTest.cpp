85242dca3577ad69728ea60d0c3d07751382ad37
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-05-08 17:25:51 +0300

    Pocinka testov na biblioteku "PCRE". Ispoljzujem jejo naprämuju, t. k. C++ obörtka ne umejet nahoditj sopostavlenija s ucötom prosmotra nazad.

--- Tests/PcreRegexMatcherTest.cpp
+++ Tests/PcreRegexMatcherTest.cpp
@@ -3,7 +3,7 @@
 #include "../RegPanzerLib/PushDisableLLVMWarnings.hpp"
 #include <gtest/gtest.h>
 #include "../RegPanzerLib/PopLLVMWarnings.hpp"
-#include <pcrecpp.h>
+#include <pcre.h>
 
 namespace RegPanzer
 {
@@ -17,29 +17,44 @@ TEST_P(PcreRegexMatchTest, TestMatch)
 {
 	const auto param= GetParam();
 
-	if((GetRegexFeatures(param.regex_str) | RegexFeatureFlag::FourDigitHexCodes) != 0)
+	if((GetRegexFeatures(param.regex_str) & RegexFeatureFlag::FourDigitHexCodes) != 0)
 		return;
 
-	pcrecpp::RE_Options options;
-	options.set_utf8(true);
-	pcrecpp::RE r(param.regex_str, options);
+	const char* error_ptr= nullptr;
+	int error_offset= 0;
+	pcre* const r= pcre_compile(param.regex_str.data(), PCRE_UTF8, &error_ptr, &error_offset, nullptr);
+	ASSERT_TRUE(r != nullptr);
 
-	for(const MatcherTestDataElement::Case& c : param.cases)
+	try
 	{
-		MatcherTestDataElement::Ranges result_ranges;
-
-		pcrecpp::StringPiece str(c.input_str.data());
-		while(!str.empty())
+		int vec[100*3]{};
+		for(const MatcherTestDataElement::Case& c : param.cases)
 		{
-			const auto prev_str= str;
-			if(r.Consume(&str))
-				result_ranges.emplace_back(size_t(prev_str.data() - c.input_str.data()), size_t(str.data() - c.input_str.data()));
-			else
-				str.remove_prefix(1);
-		}
+			MatcherTestDataElement::Ranges result_ranges;
+			for(size_t i= 0; i < c.input_str.size();)
+			{
+				if(pcre_exec(r, nullptr, c.input_str.data(), int(c.input_str.size()), int(i), PCRE_NO_UTF8_CHECK, vec, std::size(vec)) != 0)
+				{
+					if(vec[0] < 0 || vec[1] < 0 || size_t(vec[1]) <= i || size_t(vec[1]) > c.input_str.size())
+						break;
 
-		EXPECT_EQ(result_ranges, c.result_ranges);
+					result_ranges.emplace_back(size_t(vec[0]), size_t(vec[1]));
+					i= size_t(vec[1]);
+				}
+				else
+					++i;
+			}
+
+			EXPECT_EQ(result_ranges, c.result_ranges);
+		}
 	}
+	catch(...)
+	{
+		pcre_free(r);
+		throw;
+	}
+
+	pcre_free(r);
 }
 
 INSTANTIATE_TEST_CASE_P(M, PcreRegexMatchTest, testing::ValuesIn(g_matcher_test_data));
