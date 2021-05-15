5ad5cf3cb5617540415bc253af404450ef668172
Panzerschrek <AntiPanzerschrek@yandex.ru>
2021-05-15 10:23:45 +0300

    Napisany testy na bezuslovnoje izvlecenije grupp.

--- Tests/GroupsExtractionTestData.cpp
+++ Tests/GroupsExtractionTestData.cpp
@@ -80,6 +80,76 @@ const GroupsExtractionTestDataElement g_groups_extraction_test_data[]
 			},
 		}
 	},
+
+	{ // Extract two sequential groups.
+		"([a-z]+)([0-9]+)",
+		{
+			{ // Empty string - no matches.
+				"",
+				{},
+			},
+			{ // Simplest possible match.
+				"z3",
+				{ { {0, 2}, {0, 1}, {1, 2} } }
+			},
+			{ // Match for long sequences.
+				"hjahntt43672",
+				{ { {0, 12}, {0, 7}, {7, 12} } }
+			},
+			{ // Two sequential matches.
+				"abc54nmytr661",
+				{ { {0, 5}, {0, 3}, {3, 5} }, { {5, 13}, {5, 10}, {10, 13} } }
+			},
+			{ // Two matches in long string.
+				"!$afr4 += hjy45;",
+				{ { {2, 6}, {2, 5}, {5, 6} }, { {10, 15}, {10, 13}, {13, 15} } }
+			},
+		}
+	},
+
+	{ // Extract two groups - one is inside another.
+		"\\((Q([A-Z]+)W)\\)",
+		{
+			{ // Empty string - no matches.
+				"",
+				{},
+			},
+			{ // No math - missing required symbol in sequence.
+				"(QW)",
+				{},
+			},
+			{ // Simplest possible match.
+				"(QBW)",
+				{ { {0, 5}, {1, 4}, {2, 3} } },
+			},
+			{ // Match with long sequence.
+				"(QGIONDMOMW)",
+				{ { {0, 12}, {1, 11}, {2, 10} } },
+			},
+			{ // Three sequential matches.
+				"(QMW)(QHNW)(QQWW)",
+				{ { {0, 5}, {1, 4}, {2, 3} }, { {5, 11}, {6, 10}, {7, 9} }, { {11, 17}, {12, 16}, {13, 15} } },
+			},
+		}
+	},
+
+	{ // Several groups - sequential and nested.
+		"<(A([0-9])(([a-z]+)=(\\4))Z);([a-f]+)>",
+		{
+			{ // Empty string - no matches.
+				"",
+				{},
+			},
+			{ // Simplest possible match.
+				"<A6s=sZ;b>",
+				{ { {0, 10}, {1, 7}, {2, 3}, {3, 6}, {3, 4}, {5, 6}, {8, 9} } }
+			},
+			{ // Long match.
+				"<A1hjtwq=hjtwqZ;bcbbadf>",
+				{ { {0, 24}, {1, 15}, {2, 3}, {3, 14}, {3, 8}, {9, 14}, {16, 23} } }
+			},
+		},
+	},
 };
 
 const size_t g_groups_extraction_test_data_size= std::size(g_groups_extraction_test_data);
