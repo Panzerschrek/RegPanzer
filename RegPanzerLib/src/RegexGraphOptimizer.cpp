b5962113988e52d36b4b5284af714a9a66d3ea60
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-07-09 09:09:41 +0300

    Popravlena beskonecnaja rekursija v kode poiska simvolov, s kotoryh nacinajetsä posledovateljnostj.

--- RegPanzerLib/src/RegexGraphOptimizer.cpp
+++ RegPanzerLib/src/RegexGraphOptimizer.cpp
@@ -7,6 +7,8 @@ namespace RegPanzer
 namespace
 {
 
+using VisitedNodesSet= std::unordered_set<GraphElements::NodePtr>;
+
 //
 // Start symbols stuff
 //
@@ -62,178 +64,191 @@ OneOf GetAnySymbol()
 	return OneOf{ {}, {}, true };
 }
 
-OneOf GetPossibleStartSybmols(const GraphElements::NodePtr& node);
+OneOf GetPossibleStartSybmols(VisitedNodesSet& visited_nodes, const GraphElements::NodePtr& node);
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::AnySymbol& any_symbol)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::AnySymbol& any_symbol)
 {
+	(void)visited_nodes;
 	(void)any_symbol;
 	// Inverted empty set.
 	// TODO - process "AnySymbol" with exclusions.
 	return OneOf{ {}, {}, true };
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::SpecificSymbol& specific_symbol)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::SpecificSymbol& specific_symbol)
 {
+	(void)visited_nodes;
 	return OneOf{ {specific_symbol.code}, {}, false };
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::String& string)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::String& string)
 {
 	const auto str_utf32= Utf8ToUtf32(string.str);
 	if(!str_utf32.empty())
 		return OneOf{ {str_utf32.front()}, {}, false };
-	return GetPossibleStartSybmols(string.next);
+	return GetPossibleStartSybmols(visited_nodes, string.next);
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::OneOf& one_of)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::OneOf& one_of)
 {
+	(void)visited_nodes;
 	return OneOf{ one_of.variants, one_of.ranges, one_of.inverse_flag };
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::Alternatives& alternatives)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::Alternatives& alternatives)
 {
-	// Hack to prevent infinite loops.
-	// TODO - fix this.
-	if(true) { return GetAnySymbol(); }
-
 	OneOf res;
 	for(const GraphElements::NodePtr& next : alternatives.next)
-		res= CombineSymbolSets(res, GetPossibleStartSybmols(next));
+		res= CombineSymbolSets(res, GetPossibleStartSybmols(visited_nodes, next));
 
 	return res;
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::AlternativesWithOptimizedBacktracking& alternatives_with_optimized_backtracking)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::AlternativesWithOptimizedBacktracking& alternatives_with_optimized_backtracking)
 {
-	// Hack to prevent infinite loops.
-	// TODO - fix this.
-	if(true) { return GetAnySymbol(); }
-
-	return CombineSymbolSets(GetPossibleStartSybmols(alternatives_with_optimized_backtracking.path0_element), GetPossibleStartSybmols(alternatives_with_optimized_backtracking.path1_next));
+	return CombineSymbolSets(
+			GetPossibleStartSybmols(visited_nodes, alternatives_with_optimized_backtracking.path0_element),
+			GetPossibleStartSybmols(visited_nodes, alternatives_with_optimized_backtracking.path1_next));
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::GroupStart& group_start)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::GroupStart& group_start)
 {
-	return GetPossibleStartSybmols(group_start.next);
+	return GetPossibleStartSybmols(visited_nodes, group_start.next);
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::GroupEnd& group_end)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::GroupEnd& group_end)
 {
-	return GetPossibleStartSybmols(group_end.next);
+	return GetPossibleStartSybmols(visited_nodes, group_end.next);
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::BackReference& back_reference)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::BackReference& back_reference)
 {
+	(void)visited_nodes;
 	(void)back_reference;
 	// Any symbol is possible in backreference.
 	 return GetAnySymbol();
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::LookAhead& look_ahead)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::LookAhead& look_ahead)
 {
+	(void)visited_nodes;
 	(void)look_ahead;
 	// Any symbol is possible in look_ahead.
 	 return GetAnySymbol();
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::LookBehind& look_behind)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::LookBehind& look_behind)
 {
+	(void)visited_nodes;
 	(void)look_behind;
 	// Any symbol is possible in look_behind.
 	 return GetAnySymbol();
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::StringStartAssertion& string_start_assertion)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::StringStartAssertion& string_start_assertion)
 {
+	(void)visited_nodes;
 	(void)string_start_assertion;
 	// Fail possessification optimization in such case - return all possible symbols.
 	 return GetAnySymbol();
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::StringEndAssertion& string_end_assertion)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::StringEndAssertion& string_end_assertion)
 {
+	(void)visited_nodes;
 	(void)string_end_assertion;
 	// Fail possessification optimization in such case - return all possible symbols.
 	 return GetAnySymbol();
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::ConditionalElement& conditional_element)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::ConditionalElement& conditional_element)
 {
 	return CombineSymbolSets(
-		GetPossibleStartSybmols(conditional_element.next_true),
-		GetPossibleStartSybmols(conditional_element.next_false));
+		GetPossibleStartSybmols(visited_nodes, conditional_element.next_true),
+		GetPossibleStartSybmols(visited_nodes, conditional_element.next_false));
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::SequenceCounterReset& sequence_counter_reset)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::SequenceCounterReset& sequence_counter_reset)
 {
-	return GetPossibleStartSybmols(sequence_counter_reset.next);
+	return GetPossibleStartSybmols(visited_nodes, sequence_counter_reset.next);
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::SequenceCounter& sequence_counter)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::SequenceCounter& sequence_counter)
 {
 	return CombineSymbolSets(
-		GetPossibleStartSybmols(sequence_counter.next_iteration),
-		GetPossibleStartSybmols(sequence_counter.next_sequence_end));
+		GetPossibleStartSybmols(visited_nodes, sequence_counter.next_iteration),
+		GetPossibleStartSybmols(visited_nodes, sequence_counter.next_sequence_end));
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::NextWeakNode& next_weak)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::NextWeakNode& next_weak)
 {
-	return GetPossibleStartSybmols(next_weak.next.lock());
+	return GetPossibleStartSybmols(visited_nodes, next_weak.next.lock());
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::PossessiveSequence& possessive_sequence)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::PossessiveSequence& possessive_sequence)
 {
 	// Combine for cases of empty sequence.
 	// TODO - return only first symbol of sequence if min size is greater than zero.
 	return CombineSymbolSets(
-		GetPossibleStartSybmols(possessive_sequence.sequence_element),
-		GetPossibleStartSybmols(possessive_sequence.next));
+		GetPossibleStartSybmols(visited_nodes, possessive_sequence.sequence_element),
+		GetPossibleStartSybmols(visited_nodes, possessive_sequence.next));
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::FixedLengthElementSequence& fixed_length_element_sequence)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::FixedLengthElementSequence& fixed_length_element_sequence)
 {
 	// Combine for cases of empty sequence.
 	return CombineSymbolSets(
-		GetPossibleStartSybmols(fixed_length_element_sequence.sequence_element),
-		GetPossibleStartSybmols(fixed_length_element_sequence.next));
+		GetPossibleStartSybmols(visited_nodes, fixed_length_element_sequence.sequence_element),
+		GetPossibleStartSybmols(visited_nodes, fixed_length_element_sequence.next));
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::AtomicGroup& atomic_group)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::AtomicGroup& atomic_group)
 {
 	// Combine for cases of empty atomic group. TODO - is this really needed? Or maybe symbols set will contain all symbols in empty group?
 	return CombineSymbolSets(
-		GetPossibleStartSybmols(atomic_group.next),
-		GetPossibleStartSybmols(atomic_group.group_element));
+		GetPossibleStartSybmols(visited_nodes, atomic_group.next),
+		GetPossibleStartSybmols(visited_nodes, atomic_group.group_element));
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::SubroutineEnter& subroutine_enter)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::SubroutineEnter& subroutine_enter)
 {
-	return GetPossibleStartSybmols(subroutine_enter.subroutine_node);
+	return GetPossibleStartSybmols(visited_nodes, subroutine_enter.subroutine_node);
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::SubroutineLeave& subroutine_leave)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::SubroutineLeave& subroutine_leave)
 {
+	(void)visited_nodes;
 	(void)subroutine_leave;
 	// Any symbol is possible after subroutine leave.
 	return GetAnySymbol();
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::StateSave& state_save)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::StateSave& state_save)
 {
-	return GetPossibleStartSybmols(state_save.next);
+	return GetPossibleStartSybmols(visited_nodes, state_save.next);
 }
 
-OneOf GetPossibleStartSybmolsImpl(const GraphElements::StateRestore& state_restore)
+OneOf GetPossibleStartSybmolsImpl(VisitedNodesSet& visited_nodes, const GraphElements::StateRestore& state_restore)
 {
-	return GetPossibleStartSybmols(state_restore.next);
+	return GetPossibleStartSybmols(visited_nodes, state_restore.next);
 }
 
-OneOf GetPossibleStartSybmols(const GraphElements::NodePtr& node)
+OneOf GetPossibleStartSybmols(VisitedNodesSet& visited_nodes, const GraphElements::NodePtr& node)
 {
 	if(node == nullptr)
 		return OneOf{}; // Empty set of symbols.
 
-	return std::visit([&](const auto& el){ return GetPossibleStartSybmolsImpl(el); }, *node);
+	if(visited_nodes.count(node) != 0)
+		return OneOf{}; // Already visited this node. This is possible for sequences with zero possible element size.
+	visited_nodes.insert(node);
+
+	return std::visit([&](const auto& el){ return GetPossibleStartSybmolsImpl(visited_nodes, el); }, *node);
+}
+
+OneOf GetPossibleStartSybmolsEntry(const GraphElements::NodePtr& node)
+{
+	VisitedNodesSet visited_nodes;
+	return GetPossibleStartSybmols(visited_nodes, node);
 }
 
 //
@@ -241,7 +256,6 @@ OneOf GetPossibleStartSybmols(const GraphElements::NodePtr& node)
 //
 
 using NodeEnumerationFunction= std::function<void(const GraphElements::NodePtr)>;
-using VisitedNodesSet= std::unordered_set<GraphElements::NodePtr>;
 
 void EnumerateAllNodesOnceImpl(
 	const NodeEnumerationFunction& func,
@@ -509,7 +523,7 @@ void ApplyAlternativesBacktrackingEliminationOptimizationToNode(const GraphEleme
 	const GraphElements::NodePtr first_alternative= alternatives->next[0];
 	const GraphElements::NodePtr second_alternative= alternatives->next[1];
 
-	if(HasIntersection(GetPossibleStartSybmols(first_alternative), GetPossibleStartSybmols(second_alternative)))
+	if(HasIntersection(GetPossibleStartSybmolsEntry(first_alternative), GetPossibleStartSybmolsEntry(second_alternative)))
 		return;
 
 	// Create copy of first alternative node in order to avoid modifying existing node.
